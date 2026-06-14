dc0fdd454a1b642d3eff2f1f7910daf9582b8177
nishadbasrur <nishadbasrur@gmail.com>
2026-06-14 16:40:41 -0400

    Build goals screen

---
+++ app/goals/page.tsx
@@ -0,0 +1,5 @@
+import { ResourcePage } from "@/components/domain/resource-page";
+
+export default function GoalsPage() {
+  return <ResourcePage title="Goals" description="Long-term outcomes and strategic direction." />;
+}
