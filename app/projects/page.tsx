96674895ca1bfb3dacccb959febe15bc278524b4
nishadbasrur <nishadbasrur@gmail.com>
2026-06-14 16:40:55 -0400

    Build projects screen

---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+import { ResourcePage } from "@/components/domain/resource-page";
+
+export default function ProjectsPage() {
+  return <ResourcePage title="Projects" description="Multi-step efforts that advance goals." />;
+}
