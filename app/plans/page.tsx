31f0f5dd6467caf871e1a4b5263e404bee0b71ce
nishadbasrur <nishadbasrur@gmail.com>
2026-06-14 16:41:21 -0400

    Build plans screen

---
+++ app/plans/page.tsx
@@ -0,0 +1,5 @@
+import { ResourcePage } from "@/components/domain/resource-page";
+
+export default function PlansPage() {
+  return <ResourcePage title="Plans" description="Structured paths from current state to desired outcome." />;
+}
